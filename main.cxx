bdb317f628b7ce21bbaa6bd3b275dd0696d75ff5
Einar Elén <einar.elen@gmail.com>
2023-10-10 17:33:48 +0200

    Add a basic main.cxx file

---
+++ main.cxx
@@ -0,0 +1,8 @@
+
+#include <iostream>
+int main(int argc, char *argv[]) {
+  std::cout << "I am just a code template, you need to implement the "
+               "functionality you want to use yourself!"
+            << std::endl;
+  return 0;
+}
