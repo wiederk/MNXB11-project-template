7b0db181d64dbac00512509ce278447b5e358860
Einar Elén <einar.elen@gmail.com>
2023-10-15 22:56:24 +0200

    Update main to use the command line arguments

--- main.cxx
+++ main.cxx
@@ -4,5 +4,12 @@ int main(int argc, char *argv[]) {
   std::cout << "I am just a code template, you need to implement the "
                "functionality you want to use yourself!"
             << std::endl;
+
+  std::cout << "We were passed " << argc
+            << " command line arguments, the first of which was " << argv[0]
+            << std::endl;
+  std::cout << "With a good CLI library, we could use the command line "
+               "arguments to make a useful program."
+            << std::endl;
   return 0;
 }
