8ab22436599fca9d125607687a45ce78972e9e73
“shilparani <shilpa_rani.sahu@sljus.lu.se>
2023-10-24 13:37:59 +0200

    updating argumentum build directory files along with main and Makefile with added path in it

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CXX := g++
 CXXWARNINGS := -Wall -Wextra -Werror
 CXXOPT := -O3
 CXXSTD := -std=c++17
-INCLUDES := -I external/include
+INCLUDES := -I external/include  -L build/argumentum
 ROOTCONFIG :=root-config
 
 #Get ROOT flags for include paths and libraries
--- main.cxx
+++ main.cxx
@@ -1,7 +1,11 @@
 
 #include <iostream>
-#include "argumentum.h"
-#include "argh.h"
+#include <argh.h>
+#include <argumentum/argparse.h>
+
+using namespace std;
+using namespace argumentum;
+
 int main(int argc, char *argv[]) {
   std::cout << "I am just a code template, you need to implement the "
                "functionality you want to use yourself!"
@@ -13,5 +17,30 @@ int main(int argc, char *argv[]) {
   std::cout << "With a good CLI library, we could use the command line "
                "arguments to make a useful program."
             << std::endl;
+
+   //argumentum code for main function
+   //In this example the program accepts integers and 
+   //finds the largest one. If the option --sum is passed 
+   //to the program, the numbers are summed, instead.
+   
+   vector<int> numbers;
+   bool isSum = false;
+
+   auto parser = argument_parser{};
+   auto params = parser.params();
+   parser.config().program( argv[0] ).description( "Accumulator" );
+   params.add_parameter( numbers, "N" ).minargs( 1 ).metavar( "INT" ).help( "Integers" );
+   params.add_parameter( isSum, "--sum", "-s" )
+         .nargs( 0 )
+         .help( "Sum the integers (default: find the max)" );
+
+   if ( !parser.parse_args( argc, argv, 1 ) )
+      return 1;
+
+   auto mmax = []( auto&& a, auto&& b ) { return max( a, b ); };
+   auto acc = isSum ? accumulate( numbers.begin(), numbers.end(), 0 )
+                    : accumulate( numbers.begin(), numbers.end(), INT_MIN, mmax );
+   cout << acc << "\n";
+  
   return 0;
 }
